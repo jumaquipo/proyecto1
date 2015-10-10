68456dc2ee42706b08d9637217c2fedb156353fc
jumaquipo <jumaquipo@hotmail.com>
2015-10-09 23:41:41 -0500

    cambios

--- Minstruccion.h
+++ Minstruccion.h
@@ -3,6 +3,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+
+/**
+ *  \brief     Esta función muestra las instruciones en el operador 1.
+ *  \param     P operador 1 estructuta tipo instruction.
+ *  \return    No retorna valor.
+ *  \author    Juan Manuel Quintero Polania- Leader
+ *  \author    Julio Edgar Mosquera Angulo - Documenting
+ *  \version   4.1a
+ *  \date      19/09/2015
+ *  \pre       First initialize the system.
+*/
 void Minstruccion(instruction_t instruction);
+
+/**
+ *  \brief     Esta función obtiene los registros, los almacena en el operador 1.
+ *  \param     a operador 1 puntero tipo entero de 32 bits.
+ *  \return    No retorna valor.
+ *  \author    Juan Manuel Quintero Polania- Leader
+ *  \author    Julio Edgar Mosquera Angulo - Documenting
+ *  \version   4.1a
+ *  \date      19/09/2015
+ *  \pre       First initialize the system.
+*/
 void ORL(uint8_t *a);
 
---
+++ Mregistros.h
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <curses.h>
+
+/**
+ *  \brief     Esta función imprime los registros.
+ *  \param     Ptro operador 1 puntero tipo entero.
+ *  \param     n operador 2 tipo entero.
+ *  \return    No retorna valor.
+ *  \author    Juan Manuel Quintero Polania- Leader
+ *  \author    Julio Edgar Mosquera Angulo - Documenting
+ *  \version   4.1a
+ *  \date      19/09/2015
+ *  \pre       First initialize the system.
+*/
+void Mregistro(uint32_t *ptro,int n);
